8ffcf9154ac198113776b4fa15edf824e3555a54
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2020-02-19 23:49:13 +0100

    Add spec alpha definitions for regal expressions
    Can be made available by explicitly requiring
    `lambdaisland.regal.spec-alpha`.
    
    Tests now use those specs and instrument `regal/regex` to hopefully
    catch divergence of spec and implementation.

---
+++ src/lambdaisland/regal/spec_alpha.clj
@@ -0,0 +1,73 @@
+(ns lambdaisland.regal.spec-alpha
+  (:require [lambdaisland.regal :as regal]
+            [clojure.spec.alpha :as s]))
+
+(s/fdef regal/regex :args (s/cat :form ::regal/form))
+
+(s/def ::regal/form
+  (s/or :literal ::regal/literal
+        :token   ::regal/token
+        :op      ::regal/op))
+
+(s/def ::regal/literal
+  (s/or :string string?
+        :char   char?))
+
+(s/def ::regal/token simple-keyword?)
+
+(defmulti op first)
+
+(s/def ::regal/op
+  (s/and vector?
+         (s/multi-spec op (fn [v t] (cons t v)))))
+
+(defn op-spec [args-spec]
+  (s/cat :op keyword?
+         :args args-spec))
+
+(defmethod op :cat [_]
+  (op-spec (s/* ::regal/form)))
+
+(defmethod op :alt [_]
+  (op-spec (s/* ::regal/form)))
+
+(defmethod op :capture [_]
+  (op-spec (s/* ::regal/form)))
+
+(defmethod op :* [_]
+  (op-spec (s/+ ::regal/form)))
+
+(defmethod op :+ [_]
+  (op-spec (s/+ ::regal/form)))
+
+(defmethod op :? [_]
+  (op-spec (s/+ ::regal/form)))
+
+(s/def ::regal/repeat-min nat-int?)
+(s/def ::regal/repeat-max nat-int?)
+
+(defmethod op :repeat [_]
+  (op-spec (s/cat :form ::regal/form
+                  :min  ::regal/repeat-min
+                  :min  (s/? ::regal/repeat-max))))
+
+(s/def ::regal/range-start char?)
+(s/def ::regal/range-end char?)
+
+(s/def ::regal/range
+  (s/cat :start ::regal/range-start
+         :end   ::regal/range-end))
+
+(defmethod op :range [_]
+  (op-spec ::regal/range))
+
+(s/def ::regal/class
+  (s/+ (s/or :range ::regal/range
+             :char   char?
+             :string string?)))
+
+(defmethod op :class [_]
+  (op-spec ::regal/class))
+
+(defmethod op :not [_]
+  (op-spec ::regal/class))
