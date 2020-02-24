c5eb1bb9139cdbafc2eecc178c818ac605cfebeb
Arne Brasseur <arne@arnebrasseur.net>
2020-02-24 17:02:26 +0100

    .dir-locals.el : set indent information for certain forms

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,5 @@
-((nil . ((cider-clojure-cli-global-options . "-A:dev:test:cljs:test-check"))))
+((nil . ((cider-clojure-cli-global-options . "-A:dev:test:cljs:test-check:test-chuck")))
+ (clojure . ((eval . (define-clojure-indent
+                       (assoc 0)
+                       (for-all 1)
+                       (ex-info 0))))))
