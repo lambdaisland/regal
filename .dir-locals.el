2e556057d044d090e8cfb592f917664a07044ddf
Arne Brasseur <arne@arnebrasseur.net>
2020-04-29 11:27:56 +0200

    Some common sense CIDER settings

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ((nil . ((cider-clojure-cli-global-options . "-A:dev:test:cljs:spec-alpha:malli:test-check:instaparse")
+         (cider-preferred-build-tool           . clojure-cli)
+         (cider-redirect-server-output-to-repl . t)
+         (cider-repl-display-help-banner       . nil)
+         (clojure-toplevel-inside-comment-form . t)
          (cider-default-cljs-repl . node)))
  (clojure . ((eval . (define-clojure-indent
                        (assoc 0)
